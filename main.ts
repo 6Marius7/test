6cc1de8a7e43d3eb8b97dd0de5864e5359a75d88
6Marius7 <152004303+6Marius7@users.noreply.github.com>
2024-05-28 11:26:12 +0200

    Starting code

--- main.ts
+++ main.ts
@@ -1,6 +1,29 @@
 input.onButtonPressed(Button.A, function () {
 	
 })
+let LoggingData = 0
+let header_to = ""
+let logging_data = 0
+logging_data += 1
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
+while (logging_data == 0) {
+    let Header = 0
+    weatherbit.startWeatherMonitoring()
+    serial.redirect(
+    SerialPin.P15,
+    SerialPin.P14,
+    BaudRate.BaudRate9600
+    )
+    header_to = "Temperature" + "," + "Humidity" + "," + "Presure Pha" + "," + "Wind speed" + "," + "Direction" + "," + "Rain"
+    serial.writeLine("" + (Header))
+    LoggingData = 0
+}
 basic.forever(function () {
 	
 })
